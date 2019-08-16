088ad12de063b893c9ac6209e073fba3ee21197e
Michael Maleki <contact@michaelmaleki.com>
2019-08-16 12:47:48 -0400

    update master

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ''
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Header;
